ce47378eea6ac6598bebec93e5328e77eae244fb
Victor Klisman <vklisman@gmail.com>
2024-05-02 15:27:19 -0400

    Iniciando o projeto Spring

---
+++ screenmatch/src/main/java/br/com/vklisman/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.vklisman.screenmatch;
+
+import br.com.vklisman.screenmatch.model.DadosSerie;
+import br.com.vklisman.screenmatch.service.ConsumoApi;
+import br.com.vklisman.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=1fe4a160");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
