8c33fd783a5096a041cca6fbcb7ecc49b3a29186
Victor Klisman <vklisman@gmail.com>
2024-05-10 23:44:45 -0400

    feat: criando um mapa com dados por temporada

--- screenmatch/src/main/java/br/com/vklisman/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/vklisman/screenmatch/principal/Principal.java
@@ -63,18 +63,18 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
-        System.out.println("Digite um trecho do título do episódio:");
-
-        var trechoTitulo = leitura.nextLine();
-        Optional<Episodio> episodioBuscado = episodios.stream()
-                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
-                .findFirst();
-        if(episodioBuscado.isPresent()){
-            System.out.println("Episódio encontrado!");
-            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
-        } else {
-            System.out.println("Episódio não encontrado!");
-        }
+//        System.out.println("Digite um trecho do título do episódio:");
+//
+//        var trechoTitulo = leitura.nextLine();
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//                .findFirst();
+//        if(episodioBuscado.isPresent()){
+//            System.out.println("Episódio encontrado!");
+//            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+//        } else {
+//            System.out.println("Episódio não encontrado!");
+//        }
 
 //        System.out.println("A partir de que ano você deseja ver os episodios?");
 //        var ano = leitura.nextInt();
@@ -90,5 +90,11 @@ public class Principal {
 //                                " Episodio: " + e.getTitulo() +
 //                                " Data Lançamento: " + e.getDataLancamento().format(formatador)
 //                ));
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
    }
 }
