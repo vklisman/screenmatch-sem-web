1837e0a6d8d64f88c20ff2ddd83f6d59746a5da7
Victor Klisman <vklisman@gmail.com>
2024-05-10 23:55:00 -0400

    feat: coletando estatisticas com DoubleSummaryStatistics

--- screenmatch/src/main/java/br/com/vklisman/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/vklisman/screenmatch/principal/Principal.java
@@ -96,5 +96,14 @@ public class Principal {
                 .collect(Collectors.groupingBy(Episodio::getTemporada,
                         Collectors.averagingDouble(Episodio::getAvaliacao)));
         System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
+
    }
 }
